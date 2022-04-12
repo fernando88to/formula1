e1e70c531289588e655437ec50de37e73fad36a0
Fernando <fernando88to@gmail.com>
2022-04-12 10:36:09 -0300

    frontend - inicio da listagem do championshiop

---
+++ front-end-nextjs/pages/championship/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const ChampionshipHome : NextPage = () => {
+    return (
+        <div>
+            <h1>Championship Home</h1>
+        </div>
+    );
+};
+
+export default ChampionshipHome;
