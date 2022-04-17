e5c13acc9bb5c0140e7d114fd93a22dd68c19879
Fernando <fernando88to@gmail.com>
2022-04-16 22:15:58 -0300

    container do front end funcionando

--- front-end-nextjs/next.config.js
+++ front-end-nextjs/next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig}
+ *
+ * webpackDevMiddleware foi inseerido para o hot reload
+ * https://webpack.js.org/configuration/watch/#watchoptionspoll
+ * */
+
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/frontend",
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: /node_modules/
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
--- front-end-nextjs/src/http.ts
+++ front-end-nextjs/src/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-   baseURL: 'http://localhost:8080/backend',
+   baseURL: 'http://0.0.0.0:8080/backend',
 });
 
 
