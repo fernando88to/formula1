2a9bb02874afb952244ca47cc3c6e44f8cb91be0
Fernando <fernando88to@gmail.com>
2022-04-29 22:01:12 -0300

    criação do componente de tabela.

--- front-end-nextjs/src/components/LayoutBootstrap/Table.tsx
+++ front-end-nextjs/src/components/LayoutBootstrap/Table.tsx
@@ -6,7 +6,7 @@ interface Props {
     columns: Array<Column>,
     rows?: Array<any>,
     loading: boolean
-
+    itemSelected?: (key: any) => void
 }
 
 interface Column {
@@ -14,52 +14,74 @@ interface Column {
     headerName: string
 }
 
+
 export const Table = (props: Props) => {
+
+    function showLoading() {
+        return <Spinners/>
+    }
+
+    function showElementsTable() {
+        return <div id="example_wrapper" className="dataTables_wrapper dt-bootstrap5">
+            <div className="row">
+                <div className="col-sm-12">
+                    <table id="example"
+                           className="table table-striped data-table dataTable"
+                           style={{width: '100%'}} role="grid"
+                           aria-describedby="example_info">
+                        <thead>
+                        <tr role="row">
+                            {props.columns.map((item, index) => {
+                                return (
+                                    <th className="sorting" key={index}>{item.headerName}</th>
+                                )
+                            })}
+                            <th className="sorting"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+
+                            // @ts-ignore
+                            props.rows.map((item, index) => {
+
+                                return (
+                                    <tr key={index}>
+                                        {
+                                            Object.keys(item)
+                                                .filter(key => props.columns.map(column => column.field).includes(key))
+                                                .map((key, index) => {
+                                                    return (
+                                                        <td key={index}>{item[key]}</td>
+                                                    )
+                                                })
+                                        }
+
+                                        <td>
+                                            <button className="btn btn-primary"
+                                                    onClick={
+                                                        // @ts-ignore
+                                                        () => props.itemSelected(item)}>
+                                                <i className="bi bi-search"/>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    }
+
+    function showTable() {
+        return props.loading ? showLoading() : showElementsTable()
+    }
     return (
         <div>
-            {props.loading && <Spinners />}
-            {!props.loading &&  <div id="example_wrapper" className="dataTables_wrapper dt-bootstrap5">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <table id="example"
-                               className="table table-striped data-table dataTable"
-                               style={{width: '100%'}} role="grid"
-                               aria-describedby="example_info">
-                            <thead>
-                            <tr role="row">
-                                {props.columns.map((item, index) => {
-                                    return (
-                                        <th className="sorting" key={index}>{item.headerName}</th>
-                                    )
-                                })}
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-
-                                // @ts-ignore
-                                props.rows.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            {
-                                                Object.keys(item)
-                                                    .filter(key => props.columns.map(column => column.field).includes(key))
-                                                    .map((key, index) => {
-                                                        return (
-                                                            <td key={index}>{item[key]}</td>
-                                                        )
-                                                    })
-                                            }
-
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-            </div>}
+            {showTable()}
         </div>
     );
 };
--- front-end-nextjs/src/pages/championship/index.tsx
+++ front-end-nextjs/src/pages/championship/index.tsx
@@ -14,8 +14,7 @@ const ChampionshipHome: NextPage<MyProps> = ({}) => {
     const [carregando, setCarregando] = useState(true);
     const [championshipList, setChampionshipList] = useState([] as Championship[]);
     const [championshipSelected, setChampionshipSelected] = useState(null as Championship | null);
-    const [year, setYear] = useState('');
-    const fields  = [{field:'id', headerName:'ID'},{field:'year', headerName:'Year'}];
+    const fields = [{field: 'id', headerName: 'ID'}, {field: 'year', headerName: 'Year'}];
 
     useEffect(() => {
         load()
@@ -27,21 +26,35 @@ const ChampionshipHome: NextPage<MyProps> = ({}) => {
         setCarregando(false);
     }
 
-  /*  const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(year);
+    const itemSelected = (key: any) => {
+        const championshipSelected = key as Championship;
+        setChampionshipSelected(championshipSelected);
+        //console.log(championshipSelected.id + ' ' + championshipSelected.year);
     }
-    const handlerChangeYear = (event: FormEvent<HTMLInputElement>) => {
-        setYear(event.currentTarget.value);
-    }*/
 
     return (
         <LayoutBootstrap title="ChampionShip List">
             <CardPage title="Championship">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="id" className="form-label">ID</label>
+                        <input type="number" className="form-control" id="id" autoComplete='off'
+                               readOnly={true} aria-describedby="id" value={championshipSelected?.id}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="year" className="form-label">Year</label>
+                        <input type="number" className="form-control" id="year"
+                               value={championshipSelected?.year}/>
+                    </div>
+
+                </form>
+            </CardPage>
+            <CardPage title="Championship">
                 <Table
-                        columns={fields}
-                        rows={championshipList}
-                        loading={carregando}
+                    columns={fields}
+                    rows={championshipList}
+                    loading={carregando}
+                    itemSelected={itemSelected}
                 />
             </CardPage>
 
@@ -55,7 +68,8 @@ export default ChampionshipHome;
 
 export const getServerSideProps: GetServerSideProps = async () => {
 
-    return {
-        props: {},
-    };
-};
+        return {
+            props: {},
+        };
+    }
+;
